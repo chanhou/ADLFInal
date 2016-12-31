18941c6143001789129aba5120051976a5b52d57
chanhou <iepiechan@gmail.com>
2016-12-31 17:12:22 +0800

    add file to generate predict result from rnn-nlu model

--- src/slu/generate_testResult_slu.py
+++ src/slu/generate_testResult_slu.py
@@ -11,7 +11,7 @@ Finally, the predicted annotations on the English side are projected to the orig
 
 import sys
 reload(sys)
-sys.path.insert(0, './dstc5/scripts')
+sys.path.insert(0, '../dstc5/scripts')
 sys.setdefaultencoding('utf-8')
 
 import nltk
@@ -31,116 +31,6 @@ from semantic_tag_parser import SemanticTagParser
 import operator
 
 import re
-
-
-class SimpleSLU:
-    def __init__(self):
-        self.__semantic_instance_list = []
-        self.__speech_act_instance_list = []
-
-        self.__semantic_model = None
-        self.__speech_act_model = None
-
-        self.__speech_act_lb = None
-
-    def load_model(self, modelfile):
-        with open('%s.act.model' % modelfile, 'r') as f:
-            self.__speech_act_model, self.__speech_act_lb = pickle.load(f)
-
-        self.__semantic_model = CRFTagger(verbose=True)
-        self.__semantic_model.set_model_file('%s.semantic.model' % modelfile)
-
-        return True
-
-    def add_instance(self, utter, speech_act, semantic_tagged):
-        tokenized = self.__tokenize(utter, semantic_tagged)
-        if tokenized is None:
-            return False
-
-        semantic_instance = []
-        for word, (bio, tag, attrs) in tokenized:
-            if bio is None:
-                sem_label = 'O'
-            else:
-                cat = None
-                for attr, val in attrs:
-                    if attr == 'cat':
-                        cat = val
-                sem_label = '%s-%s_%s' % (bio, tag, cat)
-            semantic_instance.append((unicode(word.lower()), unicode(sem_label)))
-        self.__semantic_instance_list.append(semantic_instance)
-
-        sa_label_list = []
-        for sa in speech_act:
-            sa_labels = ['%s_%s' % (sa['act'], attr) for attr in sa['attributes']]
-            sa_label_list += sa_labels
-
-        sa_label_list = sorted(set(sa_label_list))
-
-        word_feats = ' '.join([word.lower() for word, _ in tokenized])
-        self.__speech_act_instance_list.append((word_feats, sa_label_list))
-
-        return True
-
-    def train(self, modelfile):
-        sa_feats = [x for x, _ in self.__speech_act_instance_list]
-        sa_labels = [y for _, y in self.__speech_act_instance_list]
-
-        self.__speech_act_lb = preprocessing.MultiLabelBinarizer()
-        sa_labels = self.__speech_act_lb.fit_transform(sa_labels)
-
-        self.__speech_act_model = Pipeline([
-            ('vectorizer', CountVectorizer()),
-            ('tfidf', TfidfTransformer()),
-            ('clf', OneVsRestClassifier(LinearSVC(verbose=True)))])
-
-        self.__speech_act_model.fit(sa_feats, sa_labels)
-
-        with open('%s.act.model' % modelfile, 'wb') as f:
-            pickle.dump((self.__speech_act_model, self.__speech_act_lb), f)
-
-        self.__semantic_model = CRFTagger(verbose=True)
-        self.__semantic_model.train(self.__semantic_instance_list, '%s.semantic.model' % modelfile)
-
-    def pred(self, utter):
-        tokenized = self.__tokenize(utter)
-        word_feats = ' '.join([word.lower() for word, _ in tokenized])
-
-        pred_act = self.__speech_act_lb.inverse_transform(self.__speech_act_model.predict([word_feats]))
-        pred_semantic = self.__semantic_model.tag([word.lower() for word, _ in tokenized])
-
-        return (pred_act, pred_semantic)
-
-    def __tokenize(self, utter, semantic_tagged=None):
-        result = None
-        if semantic_tagged is None:
-            result = [(word, None) for word in nltk.word_tokenize(utter)]
-        else:
-            parser_raw = SemanticTagParser(False)
-            parser_tagged = SemanticTagParser(False)
-
-            segmented = ' '.join(nltk.word_tokenize(utter))
-            tagged = ' '.join(semantic_tagged)
-
-            parser_raw.feed(segmented)
-            parser_tagged.feed(tagged)
-
-            raw_chr_seq = parser_raw.get_chr_seq()
-            raw_space_seq = parser_raw.get_chr_space_seq()
-
-            tagged_chr_seq = parser_tagged.get_chr_seq()
-            tagged_space_seq = parser_tagged.get_chr_space_seq()
-
-            if raw_chr_seq == tagged_chr_seq:
-                merged_space_seq = [
-                    x or y for x, y in zip(raw_space_seq, tagged_space_seq)]
-
-                word_seq = parser_tagged.tokenize(merged_space_seq)
-                tag_seq = parser_tagged.get_word_tag_seq()
-
-                result = [(word, tag) for word, tag in zip(word_seq, tag_seq)]
-
-        return result
 
 
 class DirectLabelProjection:
@@ -234,26 +124,22 @@ def main(argv):
     parser.add_argument('--trainset', dest='trainset', action='store', metavar='TRAINSET', required=True, help='The training dataset')
     parser.add_argument('--testset', dest='testset', action='store', metavar='TESTSET', required=True, help='The test dataset')
     parser.add_argument('--dataroot', dest='dataroot', action='store', required=True, metavar='PATH',  help='Will look for corpus in <destroot>/<dataset>/...')
+    parser.add_argument('--testIntent', dest='testIntent', action='store', required=True, metavar='PATH',  help='File that rnn-nlu model predict')
+    parser.add_argument('--testSlot', dest='testSlot', action='store', required=True, metavar='PATH',  help='File that rnn-nlu model predict')
     parser.add_argument('--modelfile', dest='modelfile', action='store', required=True, metavar='MODEL_FILE',  help='File to write with trained model')
     parser.add_argument('--outfile', dest='outfile', action='store', required=True, metavar='JSON_FILE',  help='File to write with SLU output')
     parser.add_argument('--roletype', dest='roletype', action='store', choices=['GUIDE',  'TOURIST'], required=True,  help='Target role')
 
     args = parser.parse_args()
 
-    slu = SimpleSLU()
-
-    trainset = dataset_walker.dataset_walker(args.trainset, dataroot=args.dataroot, labels=True, translations=True)
-    sys.stderr.write('Loading training instances ... ')
-    for call in trainset:
-        for (log_utter, translations, label_utter) in call:
-            if (log_utter['speaker'] == 'Guide' and args.roletype == 'GUIDE') or (log_utter['speaker'] == 'Tourist' and args.roletype == 'TOURIST'):
-                slu.add_instance(log_utter['transcript'], label_utter['speech_act'], label_utter['semantic_tagged'])
-    sys.stderr.write('Done\n')
-
-    slu.train(args.modelfile)
 
     projection = DirectLabelProjection()
 
+    testIntent = args.testIntent
+    intent = open(testIntent,'r')
+    testSlot = args.testSlot
+    slot = open(testSlot,'r')
+    
     output = {'sessions': []}
     output['dataset'] = args.testset
     output['task_type'] = 'SLU'
@@ -265,11 +151,13 @@ def main(argv):
     for call in testset:
         this_session = {"session_id": call.log["session_id"], "utterances": []}
         for (log_utter, translations, label_utter) in call:
-            if (log_utter['speaker'] == 'Guide' and args.roletype == 'GUIDE') or (log_utter['speaker'] == 'Tourist' and args.roletype == 'TOURIST'):
+            if (log_utter['speaker'] == 'Guide' and args.roletype == 'GUIDE'):
                 slu_result = {'utter_index': log_utter['utter_index']}
                 if len(translations['translated']) > 0:
                     top_hyp = translations['translated'][0]['hyp']
-                    pred_act, pred_semantic = slu.pred(top_hyp)
+                    pred_act = intent.readline()
+                    pred_semantic = slot.readline()
+                    #pred_act, pred_semantic = slu.pred(top_hyp)
 
                     combined_act = {}
                     for act_label in reduce(operator.add, pred_act):
